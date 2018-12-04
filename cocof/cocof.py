b34383c40b77bd66ba0935d9829966bdc73e3d6a
Rezart Qelibari <it@astzweig.de>
2018-12-04 11:04:50 +0100

    Add filepath argument

--- cocof/cocof.py
+++ cocof/cocof.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 import click
 
+
 @click.command()
-def cli():
+@click.argument('filepath', type=click.Path(
+        exists=True,
+        dir_okay=False,
+        writable=True,
+        resolve_path=True
+    ))
+def cli(filepath):
+    """Cocof runs it's modifications on the configuration file given with
+    the 'filepath' argument."""
     pass
+
 
 if __name__ == '__main__':
     cli()
