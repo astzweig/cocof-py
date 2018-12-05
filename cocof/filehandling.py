3eccd5f5087af254e2789ea63f1f6c970e4a8c44
Rezart Qelibari <it@astzweig.de>
2018-12-05 08:43:10 +0100

    Add filehandling.parse_file

--- cocof/filehandling.py
+++ cocof/filehandling.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
+import os
 import tomlkit as TOML
 import json as JSON
 from ruamel.yaml import YAML
+
+
+def getFileExt(filepath):
+    filename, file_ext = os.path.splitext(filepath)
+
+    # Standarize extensions
+    if file_ext == 'yml':
+        file_ext = 'yaml'
+
+    return file_ext[1:]
 
 
 def parse_content(str_to_parse, format_hint):
@@ -16,4 +27,10 @@ def parse_content(str_to_parse, format_hint):
         return YAML().load(str_to_parse)
 
 
-
+def parse_file(filepath):
+    """Read the config file into python data structures. The file at filepath
+    must be readable by this process."""
+    with open(filepath, 'r') as f:
+        filecontent = f.read()
+    file_ext = getFileExt(filepath)
+    return parse_content(filecontent, file_ext)
